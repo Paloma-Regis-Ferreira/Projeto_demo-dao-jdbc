383cd051af279100af68ba1dffd33f1b29eb7b35
“Paloma-Regis-Ferreira” <palomaregis29@gmail.com>
2021-12-17 12:45:36 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();//a classe expoe um metodo que retorna o tipo da interface mas internamente faz a implementação
+		return new SellerDaoJDBC(DB.getConnection());//a classe expoe um metodo que retorna o tipo da interface mas internamente faz a implementação
 	}
 }
